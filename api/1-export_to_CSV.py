20f0a6b18352a42bce68da8c6ba7c4ea7c64de17
melekmoalla <melekmoalla2468@gmail.com>
2023-04-11 17:13:49 +0100

    test 88

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0, extend your Python script to export data in the CSV format.
+Using what you did in the task #0, extend your
+Python script to export data in the CSV format.
 """
 
 
@@ -9,22 +10,22 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    employee_id = sys.argv[1]
+    t = sys.argv[1]
     response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + employee_id)
+        'https://jsonplaceholder.typicode.com/users/' + t)
     todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+        'https://jsonplaceholder.typicode.com/todos?userId=' + t)
 
     employee = response.json()
     todos = todos.json()
     id = employee['id']
     name = employee['username']
 
-    with open(f'{employee_id}.csv', 'w') as file:
+    with open(f'{t}.csv', 'w') as file:
         for i in todos:
-            TASK_COMPLETED_STATUS = i['completed']
+            Ta = i['completed']
             TASK_TITLE = i['title']
             TASK_COMPLETED_STATUS = i['completed']
             TASK_TITLE = i['title']
             file.write(
-                f"\"{id}\",\"{name}\",\"{TASK_COMPLETED_STATUS}\",\"{TASK_TITLE}\"\n")
+                f"\"{id}\",\"{name}\",\"{Ta}\",\"{TASK_TITLE}\"\n")
