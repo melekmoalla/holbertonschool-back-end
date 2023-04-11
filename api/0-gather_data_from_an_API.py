49daa2bb7eb77aa68470c53aeb41d91b8ee5fc2e
melekmoalla <melekmoalla2468@gmail.com>
2023-04-11 15:14:25 +0100

    test corrcte

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """
 Write a Python script that, using this REST API, for a
 given employee ID, returns information about his/her TODO
@@ -35,16 +33,17 @@ if __name__ == "__main__":
     EMPLOYEE_NAME = employee_data["name"]
 
     # Retrieve todo list for employee
+    adress = "https://jsonplaceholder.typicode.com/todos?userId="
     todos_data = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
+        f"{adress}{employee_id}").json()
     # Calculate progress
     TOTAL_NUMBER_OF_TASKS = len(todos_data)
     NUMBER_OF_DONE_TASKS = len(
-        [task for task in todos_data if task["completed"] == True])
+        [task for task in todos_data if task["completed"] is True])
     progress = f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}"
 
     # Print progress and completed tasks
     print(f"Employee {EMPLOYEE_NAME} is done with tasks({progress}):")
     for TASK_TITLE in todos_data:
-        if TASK_TITLE["completed"] == True:
+        if TASK_TITLE["completed"] is True:
             print(f"\t {TASK_TITLE['title']}")
