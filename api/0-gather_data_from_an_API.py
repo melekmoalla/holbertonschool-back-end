e44e8551e36afb8651529de329815f1047db185a
melekmoalla <melekmoalla2468@gmail.com>
2023-04-13 10:26:11 +0100

    correct true

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
-""" Gather data from an API """
-import requests as r
-from sys import argv
 
+
+"""
+Write a Python script that, using this REST API, for a
+given employee ID, returns information about his/her TODO
+list progress.
+Requirements:
+You must use urllib or requests module
+The script must accept an integer as a parameter, which is
+the employee ID
+The script must display on the standard output the employee TODO
+list progress in this exact format:
+First line: Employee EMPLOYEE_NAME is done with tasks
+(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+EMPLOYEE_NAME: name of the employee
+NUMBER_OF_DONE_TASKS: number of completed tasks
+TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum
+of completed and non-completed tasks
+Second and N next lines display the title of completed tasks:
+TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+import requests
+import sys
 if __name__ == "__main__":
-    id = argv[1]
-    todo = r.get("https://jsonplaceholder.typicode.com/todos")
-    name = r.get("https://jsonplaceholder.typicode.com/users/" + id)
-    data = todo.json()
-    done = 0
-    total = 0
-    tasks = []
-    for d in data:
-        if d.get("userId") == int(id):
-            if d.get("completed"):
-                tasks.append(d.get("title"))
-                done += 1
-            total += 1
-    print("Employee {} is done with tasks({}/{}):".format(
-          name.json().get('name'), done, total))
-    for task in tasks:
-        print("\t {}".format(task))
+    # Retrieve employee data
+    employee_id = int(sys.argv[1])
+    employee_data = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
+    EMPLOYEE_NAME = employee_data["name"]
+
+    # Retrieve todo list for employee
+    todos_data = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
+    # Calculate progress
+    TOTAL_NUMBER_OF_TASKS = len(todos_data)
+    NUMBER_OF_DONE_TASKS = len(
+        [task for task in todos_data if task["completed"] == True])
+    progress = f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}"
+
+    # Print progress and completed tasks
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks({progress}):")
+    for TASK_TITLE in todos_data:
+        if TASK_TITLE["completed"] == True:
+            print(f"\t {TASK_TITLE['title']}")
