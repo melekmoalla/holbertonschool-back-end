7982c8ad5f2919449d687d74eea7416e4adc0e5b
melekmoalla <melekmoalla2468@gmail.com>
2023-04-13 13:28:06 +0100

    test corrcet

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python
+script to export data in the JSON format.
+"""
+
+import json
+import requests
+
+
+if __name__ == '__main__':
+
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users')
+
+    employee = response.json()
+    di = {}
+    for a in employee:
+        id = a['id']
+        todos = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId=' + str(id)).json()
+        b = []
+        for s in todos:
+            lis1 = {}
+            lis1["username"] = a["username"]
+            lis1["task"] = s['title']
+            lis1["completed"] = s['completed']
+            b.append(lis1)
+        di[id] = b
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(di, file)
