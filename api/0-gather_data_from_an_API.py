c119585bd9299815602d9f2c8e787330bb5354fb
melekmoalla <melekmoalla2468@gmail.com>
2023-04-13 03:16:08 +0100

    test g

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a Python script that, using this REST API, for a
-given employee ID, returns information about his/her
-list progress.
-Requirements:
-You must use urllib or requests module
-The script must accept an integer as a parameter, which is
-the employee ID
-The script must display on the standard output the employee
-list progress in this exact format:
-First line: Employee EMPLOYEE_NAME is done with tasks
-(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-EMPLOYEE_NAME: name of the employee
-NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum
-of completed and non-completed tasks
-Second and N next lines display the title of completed tasks:
-TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-"""
-
+""" Gather data from an API """
 import requests as r
 from sys import argv
 
