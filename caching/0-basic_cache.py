d240133c9b895004e25d532451863455bea85bf7
melekmoalla <melekmoalla2468@gmail.com>
2024-09-19 12:27:12 +0100

    correct task 1

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 """ BaseCaching module
 """
+
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-
-
     def put(self, key, item):
         """ Add an item in the cache
         """
